bba709224cbbf4f3655ceb2a8e0d0c61051e0d49
Evan Coakley <evancoakley1@gmail.com>
2017-08-09 14:56:58 -0500

    selecting elements

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+  Here is a rough idea for the steps you could take:
+*/
+let player = querySelector(".player")
+let search = querySelector(".search")
+let grid = querySelector(".grid")
+let url = ("https://itunes.apple.com/search?")
+
+// 1. First select and store the elements you'll be working with
+// 2. Create your `submit` event for getting the user's search term
+// 3. Create your `fetch` request that is called after a submission
+// 4. Create a way to append the fetch results to your page
+// 5. Create a way to listen for a click that will play the song in the audio play
